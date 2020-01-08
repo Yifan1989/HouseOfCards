e1026084d1aa62f07ee5ec01bd9c7e58ee084951
Yifan1989 <j.codes1989@gmail.com>
2020-01-08 20:28:17 +1300

    nav-bottom

--- LTO/src/app/nav-bottom/nav-bottom.component.ts
+++ LTO/src/app/nav-bottom/nav-bottom.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-bottom',
-  templateUrl: './nav-bottom.component.html',
-  styleUrls: ['./nav-bottom.component.css']
+    selector: 'app-nav-bottom',
+    templateUrl: './nav-bottom.component.html',
+    styleUrls: ['./nav-bottom.component.css']
 })
 export class NavBottomComponent implements OnInit {
+    private checkNum: number = 1;
+    constructor() {}
+    ngOnInit() {}
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 
+    private firstMethod(): void{
+        console.log("first method");
+
+        this.checkNum = this.checkNum - 1;
+        this.secondMethod();
+    }
+
+    private secondMethod(): void{
+        console.log("second method");
+        if(this.checkNum != 0){
+            console.log("retrieveShipments");
+        }
+        this.checkNum = this.checkNum + 1;
+    }
 }
